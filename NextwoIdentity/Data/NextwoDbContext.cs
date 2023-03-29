ab7b23038c67d2b720d68d9bc215e7be7b3483b1
user <user@DESKTOP-ALERMIH>
2023-03-29 22:04:19 +0300

    Add project files.

---
+++ NextwoIdentity/Data/NextwoDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NextwoIdentity.Data
+{
+    public class NextwoDbContext:IdentityDbContext
+    {
+        public NextwoDbContext(DbContextOptions<NextwoDbContext> options):base(options) {
+        }
+    }
+}
